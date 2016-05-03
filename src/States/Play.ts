a7f7b05090494a34cbbcf63353ba0d6f267ee567
Chris Normansell <chris.normansell@gmail.com>
2016-05-03 22:47:17 +0100

    Fixed animation and created sample monster

--- src/States/Play.ts
+++ src/States/Play.ts
@@ -1,8 +1,10 @@
+/// <reference path="../Entities/Monsters/FireMonster.ts"/>
+
 module BattleMonsters.States {
     export class Play extends Kiwi.State {
 
         protected tilemap : Kiwi.GameObjects.Tilemap.TileMap;
-        protected character : Kiwi.GameObjects.Sprite;
+        protected character : BattleMonsters.Entities.Monsters.BaseMonster;
 
         constructor() {
             super('Play');
@@ -18,18 +20,33 @@ module BattleMonsters.States {
         */
         public create() {
             this.tilemap = new Kiwi.GameObjects.Tilemap.TileMap(this, 'tilemap', this.textures.tiles);
-            this.character = new BattleMonsters.Entities.TileEntity(
+            this.character = new BattleMonsters.Entities.Monsters.FireMonster(
                 this,
                 this.textures.character,
                 0,
-                0
+                0,
+                100
             );
 
-            this.character.animation.add('idle', [0], 0.1, false);
-            this.character.animation.add('down', [0, 1, 2, 3, 0], 0.2, false);
-            this.character.animation.add('up', [12, 13, 14, 15, 12], 0.2, false);
-            this.character.animation.add('left', [8, 9, 10, 11, 8], 0.2, false);
-            this.character.animation.add('right', [4, 5, 6, 7, 4], 0.2, false);
+            var pack = new Kiwi.Plugins.DamagePipeline.Pack({
+                value: 20,
+                tags: "WATER",
+                mode: "SUBTRACT"
+            });
+
+            console.log(this.character.damagePipeline);
+            this.character.attack(pack);
+            console.log(this.character.damagePipeline);
+
+            this.character.components.add(
+                new BattleMonsters.Components.KeyboardMovement(this, 'KeyboardMovement')
+            );
+
+            this.character.animation.add('idle', [0], 0.1, true, false);
+            this.character.animation.add('down', [0, 1, 2, 3], 0.2, true, false);
+            this.character.animation.add('up', [12, 13, 14, 15], 0.2, true, false);
+            this.character.animation.add('left', [8, 9, 10, 11], 0.2, true, false);
+            this.character.animation.add('right', [4, 5, 6, 7], 0.2, true, false);
 
             this.addChild(this.tilemap.layers[0]);
             this.addChild(this.character);
