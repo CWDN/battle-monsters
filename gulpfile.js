c5d2371b450011012da17eb4832b3e1a3d465432
Chris Normansell <chris.normansell@gmail.com>
2016-06-20 23:53:43 +0100

    Cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,16 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
 var copy = require('gulp-copy');
 var ts = require('gulp-typescript');
 
-gulp.task('default', ['scripts', 'assets', 'game'], function () {
+gulp.task('default', ['assets', 'game'], function () {
 });
-
-gulp.task('scripts', function () {
-    return gulp.src([
-      './bower_components/excalibur/dist/Excalibur.js',
-      './bower_components/excalibur-tiled/dist/excalibur-tiled.js',
-      './bower_components/easystar-0.2.3.js'
-    ])
-      .pipe(concat('Excalibur.js'))
-      .pipe(gulp.dest('./build/'));
-});
-
 
 var tsProject = ts.createProject('tsconfig.json');
 gulp.task('game', function () {
     var tsResult = tsProject.src().pipe(ts(tsProject));
 
-    return tsResult.js.pipe(gulp.dest(''));
+    return tsResult.js.pipe(gulp.dest('build'));
 });
-
 
 gulp.task('assets', function () {
     return gulp.src([
