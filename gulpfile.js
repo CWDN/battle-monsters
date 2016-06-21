ee767096d5a1eb502c6f404e1ed6722da7cb459f
Chris Normansell <chris.normansell@gmail.com>
2016-06-21 20:55:53 +0100

    Remove need for the gulp typescript compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,7 @@
 var gulp = require('gulp');
 var copy = require('gulp-copy');
-var ts = require('gulp-typescript');
 
-gulp.task('default', ['assets', 'game'], function () {
-});
-
-var tsProject = ts.createProject('tsconfig.json');
-gulp.task('game', function () {
-    var tsResult = tsProject.src().pipe(ts(tsProject));
-
-    return tsResult.js.pipe(gulp.dest('build'));
-});
+gulp.task('default', ['assets'], function () {});
 
 gulp.task('assets', function () {
     return gulp.src([
@@ -19,5 +10,5 @@ gulp.task('assets', function () {
         './assets/textures/selector.png',
         './assets/maps/arena.json'
     ])
-      .pipe(copy('./build', {prefix: 1}));
+    .pipe(copy('./build', {prefix: 1}));
 });
