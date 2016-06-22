9d3c5635b1d1458e82b8eb611d79d7dd04f887df
Chris Normansell <chris.normansell@gmail.com>
2016-06-22 23:41:48 +0100

    Rewrote server boot so systemjs handles the node requirements

--- src/Server/boot.ts
+++ src/Server/boot.ts
@@ -1,7 +1,20 @@
 /// <reference path="../../typings/index.d.ts" />
 
-export function boot(express: Function, io: SocketIO.Server) {
-    io.on("connection", function (socket: SocketIO.Socket) {
-        console.log(socket.id);
+import Express from "express";
+import Http from "http";
+import Socket_IO from "socket.io";
+
+let app = Express();
+let httpServer = Http.createServer(app);
+httpServer.listen(4000);
+console.log("Listening on port 4000");
+
+let io = Socket_IO(httpServer);
+
+io.on("connection", function (socket: SocketIO.Socket) {
+    socket.on("move", function (data) {
+        socket.broadcast.emit("move", data);
     });
-}
+});
+
+app.use(Express.static("."));
